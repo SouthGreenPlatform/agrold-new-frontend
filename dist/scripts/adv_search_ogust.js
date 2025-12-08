bb87a2bcb5b29c4efdecab174541c9866ce62756
BillGates98 <billhappi@gmail.com>
2025-12-08 13:05:34 +0100

    dist regenerated

--- dist/scripts/adv_search_ogust.js
+++
@@ -1,7 +0,0 @@
-/* 
-*
-*	Implementation of advanced search with ajax performance
-*	@info need JQuery lib
-*	Creator : J-C
-*
-*/
--- src/assets/scripts/config.ts
+++ src/assets/scripts/config.ts
@@ -1,18 +1,18 @@
-export const DEV_APP_URL: string = "http://agrold.southgreen.fr/aldp/api"; 
+export const DEV_APP_URL: string = "https://agrold-v2.southgreen.fr/"; 
 
 // "https://v2.agrold.org/agrold";
 
-// http://agrold.southgreen.fr/aldp/api/webservices
+// https://agrold.southgreen.fr/aldp/api/webservices
 
-// http://agrold.southgreen.fr/aldp/api/genes/byKeyword
+// https://agrold.southgreen.fr/aldp/api/genes/byKeyword
 
-export const WEB_APP_URL: string = DEV_APP_URL; // "http://agrold.southgreen.fr/agrold";
+export const WEB_APP_URL: string = DEV_APP_URL; // "https://agrold.southgreen.fr/agrold";
 
-export const SPAR_QL_ENDPOINT_URL: string = "http://sparql.southgreen.fr"; 
+export const SPAR_QL_ENDPOINT_URL: string = "https://sparql-v2.southgreen.fr"; //  "https://sparql.southgreen.fr"; 
 
-export const FACETED_URL:string = "http://agrold.southgreen.fr/fct/"; // "http://volvestre.cirad.fr:8890/fct/"; 
+export const FACETED_URL:string = "https://fct-v2.southgreen.fr"; // "https://agrold.southgreen.fr/fct/"; // "https://volvestre.cirad.fr:8890/fct/"; 
 
-export const A_GRO_LD_API_JSON_URL: string = DEV_APP_URL + "/webservices"; // "/api/openapi.json";
+export const A_GRO_LD_API_JSON_URL: string = DEV_APP_URL + "/api/openapi.json";
 
 export const DEFAULT_API_FORMAT: string = ".jsonld";
 
